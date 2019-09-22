18aa6063ee4bc09eb398de6d4ee7eacede4a452f
Toni <31803194+Pickausernaame@users.noreply.github.com>
2019-09-22 03:34:43 +0300

    Update Foo.go

--- handlers/Foo.go
+++ handlers/Foo.go
@@ -8,6 +8,6 @@ import (
 
 // Хендлер-функция для генерации случайного значения
 func Foo(c *gin.Context) {
-	time.Sleep(time.Duration(rand.Intn(3000)) * time.Millisecond)
+	time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond)
 	c.Status(200)
 }
